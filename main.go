4c723e0c6760e819d232c3389920ce7ea8be9a65
avikaml <avikaml@post.bgu.ac.il>
2024-09-12 18:04:49 +0300

    Final update for now.

--- main.go
+++ main.go
@@ -2,22 +2,25 @@ package main
 
 import (
 	"log"
+
 	"github.com/hajimehoshi/ebiten/v2"
 
-	"github.com/hajimehoshi/ebiten/v2/vector"
 	"image/color"
+
+	"github.com/hajimehoshi/ebiten/v2/vector"
+
+	"fmt"
+	"math/rand"
 
 	"github.com/hajimehoshi/ebiten/v2/text"
 	"golang.org/x/image/font/basicfont"
-	"fmt"
-	"math/rand"
 )
 
 const (
-	screenWidth  = 640
-	screenHeight = 480
-	ballSpeed   = 5
-	paddleSpeed  = 6 // The paddle will move 6 pixels per tick
+	screenWidth     = 640
+	screenHeight    = 480
+	ballSpeed       = 5
+	paddleSpeed     = 6 // The paddle will move 6 pixels per tick
 	increaseCounter = 5
 )
 
@@ -36,9 +39,9 @@ type Ball struct {
 }
 
 type Game struct {
-	paddle Paddle
-	ball Ball
-	score, highScore int
+	paddle                Paddle
+	ball                  Ball
+	score, highScore      int
 	increaseSpeedPerCount int
 }
 
@@ -52,7 +55,7 @@ func main() {
 	}
 	ball := Ball{
 		Object: Object{
-			X:0, Y:0, Width:15, Height:15,
+			X: 0, Y: 0, Width: 15, Height: 15,
 		},
 		dxdt: ballSpeed,
 		dydt: ballSpeed,
@@ -60,7 +63,7 @@ func main() {
 
 	g := &Game{
 		paddle: paddle,
-		ball: ball,
+		ball:   ball,
 	} // empty game struct
 	err := ebiten.RunGame(g)
 	if err != nil {
@@ -74,10 +77,10 @@ func (g *Game) Update() error {
 	g.ball.Move()
 	g.CollideWithWall()
 	g.CollideWithPaddle()
-	if g.score > 0 && g.increaseSpeedPerCount % (1+rand.Intn(5)) == 0{
+	if g.score > 0 && g.increaseSpeedPerCount%(1+rand.Intn(5)) == 0 {
 		g.ball.Move()
 		//fmt.Printf("iSPC: %d", g.increaseSpeedPerCount)
-	}else if g.score >0 && g.increaseSpeedPerCount % 10 == 0{
+	} else if g.score > 0 && g.increaseSpeedPerCount%10 == 0 {
 		g.ball.moveFast()
 	}
 	return nil
@@ -85,73 +88,73 @@ func (g *Game) Update() error {
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	// Rectangles representing the paddle and the ball
-	vector.DrawFilledRect(screen, 
-		float32(g.paddle.X), float32(g.paddle.Y), float32(g.paddle.Width), float32(g.paddle.Height), 
-		color.White, false, 
+	vector.DrawFilledRect(screen,
+		float32(g.paddle.X), float32(g.paddle.Y), float32(g.paddle.Width), float32(g.paddle.Height),
+		color.White, false,
 	)
-	vector.DrawFilledRect(screen, 
+	vector.DrawFilledRect(screen,
 		float32(g.ball.X), float32(g.ball.Y), float32(g.ball.Width), float32(g.ball.Height), color.White,
 		false,
 	)
 
 	scoreString := "Score: " + fmt.Sprint(g.score)
-	text.Draw(screen, scoreString, basicfont.Face7x13, 10 ,10, color.White)
+	text.Draw(screen, scoreString, basicfont.Face7x13, 10, 10, color.White)
 	highScoreString := "High Score: " + fmt.Sprint(g.highScore)
 	text.Draw(screen, highScoreString, basicfont.Face7x13, 10, 30, color.White)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
-	 // Controls the size of our window, called every frame. We'll keep it constant.
+	// Controls the size of our window, called every frame. We'll keep it constant.
 	return screenWidth, screenHeight
 }
 
-func (padd* Paddle) MoveOnKeyPress(){
-	if ebiten.IsKeyPressed(ebiten.KeyArrowDown){
+func (padd *Paddle) MoveOnKeyPress() {
+	if ebiten.IsKeyPressed(ebiten.KeyArrowDown) {
 		padd.Y += paddleSpeed
 	}
-	if ebiten.IsKeyPressed(ebiten.KeyArrowUp){
+	if ebiten.IsKeyPressed(ebiten.KeyArrowUp) {
 		padd.Y -= paddleSpeed
 	}
 }
 
-func (ball* Ball) Move() { // The ball moves on its own
+func (ball *Ball) Move() { // The ball moves on its own
 	/*
-	for myself:
+			for myself:
 
-	Receiver (ball *Ball):
-    This is the receiver of the method. It specifies that Move is a method for the type *Ball.
-    *Ball means Move operates on a pointer to a Ball struct. This allows the method to modify
-	 the actual instance of Ball that it is called on.
+			Receiver (ball *Ball):
+		    This is the receiver of the method. It specifies that Move is a method for the type *Ball.
+		    *Ball means Move operates on a pointer to a Ball struct. This allows the method to modify
+			 the actual instance of Ball that it is called on.
 	*/
 	ball.X += ball.dxdt
 	ball.Y += ball.dydt
 }
 
-func (ball* Ball) moveFast() {
-	ball.X += 2*ball.dxdt
-	ball.Y += 2*ball.dydt
+func (ball *Ball) moveFast() {
+	ball.X += 2 * ball.dxdt
+	ball.Y += 2 * ball.dydt
 }
 
-func(g *Game) Reset() {
+func (g *Game) Reset() {
 	g.ball.X = 0
 	g.ball.Y = 0
 
 	g.score = 0
 }
 
-func (g *Game) CollideWithWall(){
-	if g.ball.X >= screenWidth{
+func (g *Game) CollideWithWall() {
+	if g.ball.X >= screenWidth {
 		g.Reset()
-	}else if g.ball.X <=0{
+	} else if g.ball.X <= 0 {
 		g.ball.dxdt = ballSpeed
-	}else if g.ball.Y <=0{
+	} else if g.ball.Y <= 0 {
 		g.ball.dydt = ballSpeed
-	}else if g.ball.Y >= screenHeight{
-		g.ball.dydt =- ballSpeed
+	} else if g.ball.Y >= screenHeight {
+		g.ball.dydt = -ballSpeed
 	}
 }
 
-func (g *Game) CollideWithPaddle(){
+func (g *Game) CollideWithPaddle() {
 	if g.ball.X+g.ball.Width >= g.paddle.X && g.ball.X < g.paddle.X+g.paddle.Width && g.ball.Y >= g.paddle.Y && g.ball.Y <= g.paddle.Y+g.paddle.Height {
 		g.ball.dxdt = -g.ball.dxdt
 		g.ball.X = g.paddle.X - g.ball.Width // Reset ball position to the front of the paddle
